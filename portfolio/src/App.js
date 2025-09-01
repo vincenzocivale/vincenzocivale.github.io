7d1c5e62ea2b7244c0adce68b9c847974c4fe6c9
Yuto17 <civalevincenzoyuto@gmail.com>
2025-09-01 16:36:22 +0200

    feat: implement theme toggle functionality and update CSS variables for light and dark themes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../src/components/Navbar';
 import Home from '../src/components/Home';
 import About from '../src/components/About';
@@ -8,9 +8,22 @@ import Contact from '../src/components/Contact';
 import './styles/main.css';
 
 const App = () => {
+  const [theme, setTheme] = useState('dark'); // Stato per gestire il tema
+
+  useEffect(() => {
+    // Applica la classe 'light-theme' al body se il tema è 'light'
+    document.body.classList.toggle('light-theme', theme === 'light');
+  }, [theme]);
+
+  // Funzione per cambiare il tema
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      {/* Passa la funzione e lo stato del tema al componente Navbar */}
+      <Navbar toggleTheme={toggleTheme} currentTheme={theme} />
       <main>
         <Home />
         <About />
