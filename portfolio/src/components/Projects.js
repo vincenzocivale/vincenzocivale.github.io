ac310390a2eafbc458470fc087228220fe73e477
Yuto17 <civalevincenzoyuto@gmail.com>
2025-12-08 14:45:05 +0100

    Add styles for project section and cards in Projects.css
    - Created styles for the projects section, including layout and typography.
    - Implemented hover effects for project cards and links.
    - Added responsive design adjustments for smaller screens.
    - Defined styles for project images, titles, descriptions, and technology lists.

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,7 +5,7 @@ const Projects = () => {
   return (
     <section id="projects" className="projects-section">
       <h2 className="projects-heading">
-        <span className="projects-number">02.</span> My Projects
+        <span className="projects-number">02.</span> <span className="gradient-text">My Projects</span>
       </h2>
 
 
@@ -37,18 +37,18 @@ const Projects = () => {
 
       <div className="project-card">
         <div className="project-image-container">
-          <img src="/assets/images/single-cell-annotation_cover.png" alt="A screenshot of Single Cell Classification Tool" className="project-image" />
+          <img src="/assets/images/IGTLInk Plugin_cover.png" alt="Cover image Plugin" className="project-image" />
         </div>
         <div className="project-content">
           <p className="project-label">Featured Project</p>
-          <h3 className="project-title">Single Cell Classification Tool</h3>
+          <h3 className="project-title">AR Medical Imaging Plugin</h3>
           <p className="project-description">
-            A tool that automatically identifies and classifies different types of cells from single-cell data. The aim is to make heterogeneous biological data easier to interpret, helping researchers to quickly discover patterns and differences between cell types.
+            This Unity plugin enables real-time visualization of 3D volumes, meshes, and surfaces from medical imaging software, connecting Unity AR applications with surgical planning tools via the widely adopted IGTLink protocol.
           </p>
           <ul className="project-tech-list">
-            <li>Pytorch</li>
-            <li>Scanpy</li>
-            <li>Transformers</li>
+            <li>Unity</li>
+            <li>Hololens2</li>
+            <li>IGTLink</li>
           </ul>
           {/* <div className="project-links">
             <a href="#" target="_blank" rel="noopener noreferrer">
