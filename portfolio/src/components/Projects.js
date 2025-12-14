661072356d2b6f567cebef30568d1813197894a3
Yuto17 <civalevincenzoyuto@gmail.com>
2025-12-14 11:08:54 +0100

    Refactor code structure for improved readability and maintainability

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -17,7 +17,7 @@ const Projects = () => {
           <p className="project-label">Featured Project</p>
           <h3 className="project-title">Single Cell Annotation Tool</h3>
           <p className="project-description">
-            A tool that automatically identifies and classifies different types of cells from single-cell data. The aim is to make heterogeneous biological data easier to interpret, helping researchers to quickly discover patterns and differences between cell types.
+            An automated single-cell annotation tool designed to identify and classify cell populations from high-dimensional datasets. By decoding biological heterogeneity, the system simplifies data interpretation, allowing researchers to swiftly identify latent patterns and distinct cellular phenotypes.
           </p>
           <ul className="project-tech-list">
             <li>Pytorch</li>
@@ -43,12 +43,38 @@ const Projects = () => {
           <p className="project-label">Featured Project</p>
           <h3 className="project-title">AR Medical Imaging Plugin</h3>
           <p className="project-description">
-            This Unity plugin enables real-time visualization of 3D volumes, meshes, and surfaces from medical imaging software, connecting Unity AR applications with surgical planning tools via the widely adopted IGTLink protocol.
+            A custom Unity plugin designed for real-time medical interoperability. It uses the OpenIGTLink protocol to establish a seamless bridge between surgical planning software and AR environments, enabling the low-latency streaming and visualisation of complex 3D volumes.
           </p>
           <ul className="project-tech-list">
             <li>Unity</li>
             <li>Hololens2</li>
             <li>IGTLink</li>
+          </ul>
+          {/* <div className="project-links">
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <img src="/assets/images/github.svg" alt="GitHub" />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer">
+              <img src="/assets/images/link.svg" alt="External Link" />
+            </a>
+          </div> */}
+        </div>
+      </div>
+
+      <div className="project-card">
+        <div className="project-image-container">
+          <img src="/assets/images/histological_image_pruning_cover.png" alt="Cover image" className="project-image" />
+        </div>
+        <div className="project-content">
+          <p className="project-label">Featured Project</p>
+          <h3 className="project-title">HistoFocus</h3>
+          <p className="project-description">
+            An optimised Vision Transformer (ViT) framework for histopathological tumour diagnosis, which uses Upper Confidence Bound (UCB) scores to dynamically prune redundant tokens. This approach accelerates cancer detection workflows by 5x while maintaining state-of-the-art accuracy
+          </p>
+          <ul className="project-tech-list">
+            <li>Pytorch</li>
+            <li>Transformers</li>
+            <li>Reinforcement Learning</li>
           </ul>
           {/* <div className="project-links">
             <a href="#" target="_blank" rel="noopener noreferrer">
